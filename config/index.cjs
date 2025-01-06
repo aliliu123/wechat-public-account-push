002e57d54ff537da0cc7a38f46c8ed35a49bf243
aliliu123 <101972619+aliliu123@users.noreply.github.com>
2025-01-06 18:27:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1izq6-Xl94IR1uydmtOv72DXEzQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2C0vQ4Idd4DR4oXnKOsu8G3OktUeHwY23nL0p357x1E',
+      useTemplateId: 'xe_mNYn1AGK3Epk9edTobVSbphoJOY95ByiE3WZdXIc',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
