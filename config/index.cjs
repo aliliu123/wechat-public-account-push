e31a821f4338385a6ff661e38dbef7f5fd740b78
aliliu123 <101972619+aliliu123@users.noreply.github.com>
2025-01-06 17:22:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,32 +24,23 @@ const USER_CONFIG = {
       id: '今天刷leetcode了吗',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '1999-03-28',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '199', date: '03-28',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '04-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-04-22' },
       ],
     },
   ],
-
-
+  
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '	-fEE5hGUbmGboBbehWr0rXgSPxrEZAMXP6G20g6E0ek',
 
